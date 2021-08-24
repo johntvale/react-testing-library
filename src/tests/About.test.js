8f939db301a5585802381a40f8bb4c0bed80eb88
johntvale <johntvale@gmail.com>
2021-08-24 19:22:56 -0400

    Requisito 2 Resolvido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import { About } from '../components';
+
+describe('Teste o componente About', () => {
+  test('Testa se a página contém um H2', () => {
+    renderWithRouter(<About />);
+    const titulo = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(titulo).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém dois parágrafos', () => {
+    renderWithRouter(<About />);
+    const num = 2;
+    const paragrafos = screen.getAllByText(/Pokémons/i);
+    expect(paragrafos).toHaveLength(num);
+  });
+  test('Testa se a página contém uma imagem', () => {
+    renderWithRouter(<About />);
+    const imagem = screen.getByRole('img');
+    expect(imagem).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

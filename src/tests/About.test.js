be3adefc431a68ad1cd9d14889e3cd1e8b8c9a16
johntvale <johntvale@gmail.com>
2021-08-24 19:25:12 -0400

    Correçao de lint requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from './utils/renderWithRouter';
 import { About } from '../components';
 
